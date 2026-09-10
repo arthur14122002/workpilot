dcdb6ed4e0ba7e3911ba79a6c06be7a74b965da5
Arthur Neuhaus <arthur@exposifyapp.com>
2026-09-10 22:11:56 +0200

    ...

--- public/js/emails.js
+++ public/js/emails.js
@@ -858,12 +858,6 @@ deleteButton.className =
 deleteButton.textContent =
     "Ordner löschen";
 
-console.log(
-    "🧪 DELETE BUTTON WIRD ERZEUGT",
-    folderName
-);
-
-
 folderMenu.appendChild(
     renameButton
 );
